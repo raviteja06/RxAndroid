b7ee886e3942e1a6fa2801366edab222e5f5d354
DESKTOP-BC37U82\Raviteja <raviteja@huginnovations.com>
2016-09-21 12:12:42 +0530

    Commit

--- app/src/main/java/demo/rxandroid/MainActivity.java
+++ app/src/main/java/demo/rxandroid/MainActivity.java
@@ -1,13 +1,19 @@
 package demo.rxandroid;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import demo.rxandroid.databinding.ActivityMainBinding;
+import rx.observers.Observers;
+import rx.subjects.PublishSubject;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        
     }
 }
